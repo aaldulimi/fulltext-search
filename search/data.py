daec386327f018939265840f6c719ecf7c0c14a8
Ahmed Al-Dulimi <ahmed.aldulimi@student.unimelb.edu.au>
2022-04-16 18:13:55 +1000

    writing data to xml document

--- search/data.py
+++ search/data.py
@@ -4,12 +4,21 @@ import datetime
 import asyncio
 import aiohttp
 from document import Document
+import random
+import xml.dom.minidom
+import xml.etree.ElementTree as ET
 
+
+ARTICLE_LIMIT = 100
+
+headers = {
+    'user-agent': f'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.{random.randint(0, 9999)} Safari/537.{random.randint(0, 99)}'  
+}
 
 all_days = []
 
 base_url = 'https://www.nytimes.com/sitemap/2021/'
-response = requests.get(base_url)
+response = requests.get(base_url, headers=headers)
 
 for node in HTMLParser(response.text).css('div > ol > li'):
     url = base_url + node.child.attrs['href']
@@ -21,31 +30,24 @@ for node in HTMLParser(response.text).css('div > ol > li'):
 
 print('FETCH IS DONE')
 
-"""
-async def fetch_articles():
-    total = 0
-    day = 0
-    for day_url in all_days:
-        day += 1
-        response = requests.get(day_url)
-        for node in HTMLParser(response.text).css('div > ul:nth-child(4) > li'):
-            article_url = node.child.attrs['href']
-            if node.child.text() == "Read the document": continue
-            if article_url.find('.com/interactive') != -1 or article_url.find('books/review/') != -1: continue
+article_count = 0
+async def get_article_data(session, url):
+    async with session.get(url) as resp:
+        response_text = await resp.text()
+        
+        if response_text:            
+            article = HTMLParser(response_text)
 
-            response = requests.get(article_url)
-            article = HTMLParser(response.text)
+            title = article.css_first('h1').text()
 
-            # title = article.css_first('h1').text()
-
-            # time:not(.header)
             date = article.css_first('time')
             date = date.attributes['datetime']
             if 'Z' in date: date = date[:date.find('.000Z')] + '+00:00'
             date = datetime.datetime.fromisoformat(date)
-            
+            date = date.strftime('%Y-%m-%d %H:%M:%S')
+
             author = article.css_first('span.last-byline')
-            if author == None: author = 'NYTimes_staff' 
+            if author == None: author = 'unkown' 
             else: author = author.child.text()
 
             body = ''
@@ -53,28 +55,28 @@ async def fetch_articles():
                 paragraph = node.child.select('p').matches
                 for p in paragraph:
                     if p: body += p.text()
-                
+
             
-            total += 1
+            doc = ET.Element('doc')
+            ET.SubElement(doc, "title").text = title
+            ET.SubElement(doc, "body").text = body
+            ET.SubElement(doc, "author").text = author
+            ET.SubElement(doc, "datetime").text = date
+            ET.SubElement(doc, "url").text = url
 
-            print(f'DAY {day:02}: {total:03}')
+            dom = xml.dom.minidom.parseString(ET.tostring(doc))
+            xml_string = dom.toprettyxml()
             
+            part1, part2 = xml_string.split('?>')
+            print(part2)
+
+            with open("data.xml", 'a') as file:
+                file.write(part2)
+                file.close()
 
 
-asyncio.run(fetch_articles())
-"""
-article_count = 0
-article_limit = 100
+            # print(article_count, title)
 
-
-async def get_article_data(session, url, article_count):
-    async with session.get(url) as resp:
-        # This is where each article is broken down and yielded        
-        response_text = await resp.text()
-        
-        if response_text:
-            #global article_count
-            print(article_count, url)
         return 
 
 async def get_article_url(session, url):
@@ -86,17 +88,20 @@ async def get_article_url(session, url):
             if node.child.text() == "Read the document": continue
             article_url = node.child.attrs['href']
             if article_url.find('.com/interactive') != -1 or article_url.find('books/review/') != -1: continue
-
+            
             global article_count
-             
-            if article_count < article_limit:
+
+            if article_count < ARTICLE_LIMIT: 
                 article_count += 1
-                all_articles.append(asyncio.ensure_future(get_article_data(session, article_url, article_count)))
+                all_articles.append(asyncio.ensure_future(get_article_data(session, article_url)))
+
 
         collection = await asyncio.gather(*all_articles)
 
+         
+
 async def main():
-    async with aiohttp.ClientSession() as session:
+    async with aiohttp.ClientSession(headers=headers) as session:
         tasks = []
         for day_url in all_days: 
             tasks.append(asyncio.ensure_future(get_article_url(session, day_url)))
--- search/playground.py
+++ search/playground.py
@@ -1,6 +1,24 @@
-date = '2021-01-01T10:00:07.000Z'
+import requests
+from selectolax.parser import HTMLParser
+import datetime
 
-date.replace('.000Z', '+00:00')
+header = {
+    'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Mobile Safari/537.36'
+}
+response = requests.get('https://www.nytimes.com/2022/04/15/world/europe/ukraine-russia-occupation.html', headers=header)
+#print(response.text)
+article = HTMLParser(response.text)
 
-print(date[:date.find('.000Z')] + '+00:00')
-print(date)
+title = article.css_first('h1')
+author = article.css_first('span.last-byline')
+if author == None: author = 'unkown' 
+else: author = author.child.text()
+
+date = article.css_first('time')
+date = date.attributes['datetime']
+if 'Z' in date: date = date[:date.find('.000Z')] + '+00:00'
+date = datetime.datetime.fromisoformat(date)
+
+
+
+print(date.strftime('%Y-%m-%d %H:%M:%S'))
